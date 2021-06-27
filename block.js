3b0dce293079e96fb064af01491b420f06d76f49
mega-yadoran <yosuke.tanaka.my@gmail.com>
2021-06-27 12:47:15 +0900

    blockのダブルクォート統一

--- block.js
+++ block.js
@@ -1,6 +1,6 @@
 exports.END_MODAL = {
     "type": "modal",
-    "callback_id": 'end',
+    "callback_id": "end",
     "submit": {
         "type": "plain_text",
         "text": "作業終了"
@@ -26,7 +26,7 @@ exports.END_MODAL = {
 
 exports.START_MODAL = {
     "type": "modal",
-    "callback_id": 'start',
+    "callback_id": "start",
     "submit": {
         "type": "plain_text",
         "text": "作業開始"
@@ -42,10 +42,10 @@ exports.START_MODAL = {
     "blocks": [
         {
             "type": "input",
-            "block_id": 'block_goal',
+            "block_id": "block_goal",
             "element": {
                 "type": "plain_text_input",
-                "action_id": 'goal',
+                "action_id": "goal",
                 "placeholder": {
                     "type": "plain_text",
                     "text": "例：技術書第3章まで読む, アップロード機能実装"
@@ -58,10 +58,10 @@ exports.START_MODAL = {
         },
         {
             "type": "input",
-            "block_id": 'block_estimated_end_time',
+            "block_id": "block_estimated_end_time",
             "element": {
                 "type": "timepicker",
-                "action_id": 'estimated_end_time',
+                "action_id": "estimated_end_time",
                 "placeholder": {
                     "type": "plain_text",
                     "text": "hh:mmで入力"
@@ -76,35 +76,35 @@ exports.START_MODAL = {
 };
 
 exports.ALERT_BUTTON_FIELD = {
-    text: '作業を終了しますか？',
-    fallback: '終了報告または延長に失敗しました。作業が既に終了しているかもしれません',
-    callback_id: 'alert_button',
-    color: '#3AA3E3',
-    attachment_type: 'default',
-    actions: [
+    "text": "作業を終了しますか？",
+    "fallback": "終了報告または延長に失敗しました。作業が既に終了しているかもしれません",
+    "callback_id": "alert_button",
+    "color": "#3AA3E3",
+    "attachment_type": "default",
+    "actions": [
         {
-            name: 'work',
-            text: '終了する',
-            type: 'button',
-            value: 'finish'
+            "name": "work",
+            "text": "終了する",
+            "type": "button",
+            "value": "finish"
         },
         {
-            name: 'work',
-            text: '1時間延長する',
-            type: 'button',
-            value: 'extend-one-hour'
+            "name": "work",
+            "text": "1時間延長する",
+            "type": "button",
+            "value": "extend-one-hour"
         },
         {
-            name: 'work',
-            text: '2時間延長する',
-            type: 'button',
-            value: 'extend-two-hour'
+            "name": "work",
+            "text": "2時間延長する",
+            "type": "button",
+            "value": "extend-two-hour"
         },
         {
-            name: 'work',
-            text: '3時間延長する',
-            type: 'button',
-            value: 'extend-three-hour'
+            "name": "work",
+            "text": "3時間延長する",
+            "type": "button",
+            "value": "extend-three-hour"
         },
     ]
 };
